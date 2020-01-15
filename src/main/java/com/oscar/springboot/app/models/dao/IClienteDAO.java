a5eabd5dae9acd768e363a482f09d6dcf7a8352a
Oscar Luna <oscarluna.dev93@gmail.com>
2020-01-14 19:33:36 -0600

    se cambia la implementacion de persistencia por CrudRepository

--- src/main/java/com/oscar/springboot/app/models/dao/IClienteDAO.java
+++ src/main/java/com/oscar/springboot/app/models/dao/IClienteDAO.java
@@ -1,13 +1,8 @@
 package com.oscar.springboot.app.models.dao;
 
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 import com.oscar.springboot.app.models.entity.Cliente;
 
-public interface IClienteDAO {
-	
-	public  List<Cliente> findAll();
-	public void save(Cliente cliente);
-	public Cliente findOne(Long id);
-	public void delete(Long id);
+public interface IClienteDAO extends CrudRepository<Cliente, Long>{
 }
