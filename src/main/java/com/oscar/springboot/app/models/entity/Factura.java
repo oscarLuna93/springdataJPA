26f06afcaa71c7efb294fb096812eb7ad90fbfea
Oscar Luna <oscarluna.dev93@gmail.com>
2020-01-20 12:41:54 -0600

    se crea la relacion OneToMany con ItemFactura

--- src/main/java/com/oscar/springboot/app/models/entity/Factura.java
+++ src/main/java/com/oscar/springboot/app/models/entity/Factura.java
@@ -1,15 +1,20 @@
 package com.oscar.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -32,6 +37,14 @@ public class Factura implements Serializable{
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "factura_id")
+	List<ItemFactura> items;
+	
+	public Factura() {
+		this.items = new ArrayList<ItemFactura>();
+	}
 	
 	public Long getId() {
 		return id;
@@ -62,5 +75,14 @@ public class Factura implements Serializable{
 	}
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
 	}
 }
