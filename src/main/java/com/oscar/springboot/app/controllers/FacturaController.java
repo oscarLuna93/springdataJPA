aacf3f9529522fc6fab88074f7ecc0f46e14dc1f
Oscar Luna <oscarluna.dev93@gmail.com>
2020-01-21 17:15:58 -0600

    se agrega metodo para cargar productos

--- src/main/java/com/oscar/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/oscar/springboot/app/controllers/FacturaController.java
@@ -1,17 +1,22 @@
 package com.oscar.springboot.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.oscar.springboot.app.models.entity.Cliente;
 import com.oscar.springboot.app.models.entity.Factura;
+import com.oscar.springboot.app.models.entity.Producto;
 import com.oscar.springboot.app.models.service.IClienteService;
+import com.oscar.springboot.app.models.service.IProductoService;
 
 @Controller
 @RequestMapping("/factura")
@@ -20,6 +25,9 @@ public class FacturaController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	@Autowired
+	private IProductoService productoService;
 	
 	@GetMapping("/form/{clienteId}")
 	public String crear(@PathVariable(value = "clienteId") Long clienteId
@@ -39,4 +47,10 @@ public class FacturaController {
 		
 		return "factura/form";
 	}
+	
+	@GetMapping(value = "/cargar-productos/{term}", produces = {"application/json"})
+	public @ResponseBody List<Producto> cargarProductos(@PathVariable String term) {
+		return productoService.findByNombre(term);
+	}
+	
 }
