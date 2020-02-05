043a9ff2914e2912e3212f5b3fcf64df8ae2195a
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-04 18:00:23 -0600

    pantalla de login

---
+++ src/main/java/com/oscar/springboot/app/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package com.oscar.springboot.app.controllers;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login(Model model, Principal principal, RedirectAttributes flash) {
+		
+		if (principal != null) {
+			flash.addAttribute("info", "Tienes una sesion activa");
+			return "redirect:/";
+		}
+		return "/login";
+	}
+}
