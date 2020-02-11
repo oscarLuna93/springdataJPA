e9d20b5ac05ed45c71a4cf37257bba791f58642b
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-11 17:28:24 -0600

    generacion de contraseñas con bcrypt

--- src/main/java/com/oscar/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/oscar/springboot/app/SpringBootDataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.oscar.springboot.app.models.service.IUploadService;
 
@@ -13,6 +14,11 @@ public class SpringBootDataJpaApplication implements CommandLineRunner{
 	@Autowired
 	IUploadService uploadService;
 	
+	private static final int CONTRASEÑAS_CIFRADAS = 2;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
@@ -21,6 +27,13 @@ public class SpringBootDataJpaApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		uploadService.deleteAll();
 		uploadService.init();
+		
+		String password = "12345";
+		
+		for (int i = 0; i < CONTRASEÑAS_CIFRADAS; i++) {
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}
 	}
 
 }
