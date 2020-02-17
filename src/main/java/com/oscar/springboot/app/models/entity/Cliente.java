1a796edd723961306679338ea333ae439efbef7c
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-16 21:36:45 -0600

    bug iteracion infinita, cambia formato de fecha

--- src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/oscar/springboot/app/models/entity/Cliente.java
@@ -24,7 +24,8 @@ import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 //asginar nombre de tabla
@@ -53,10 +54,11 @@ public class Cliente implements Serializable{
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date createAt;
 	
 	@OneToMany(mappedBy = "cliente" ,fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JsonIgnore
+	@JsonManagedReference
 	private List<Factura> facturas;
 	
 	@PrePersist
